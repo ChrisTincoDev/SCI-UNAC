b2dbf20e5638814554100b6b18cc7f08261b9c19
Christian Tinco Ortiz <christiantortiz02@gmail.com>
2025-10-02 00:24:47 -0500

    Cambio de pestañas añadido con Astro Transitions para mayor fluidez

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -9,4 +9,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   output: 'server',
+  adapter: netlify()
 });
